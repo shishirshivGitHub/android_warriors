3c4c87af07aee3d90cb46dcc399e00e17da2b5f1
Naresh <naresh@teqfocus.com>
2022-02-23 17:55:08 +0530

    MySixthCommit:Santosh

---
+++ app/build/generated/ap_generated_sources/debug/out/com/example/androidwarriors/BR.java
@@ -0,0 +1,9 @@
+package com.example.androidwarriors;
+
+public class BR {
+  public static final int _all = 0;
+
+  public static final int dashboardItem = 1;
+
+  public static final int dashboardViewmodel = 2;
+}
---
+++ app/build/generated/data_binding_trigger/debug/com/example/androidwarriors/DataBindingTriggerClass.java
@@ -0,0 +1,4 @@
+package com.example.androidwarriors;
+
+@androidx.databinding.BindingBuildInfo
+public class DataBindingTriggerClass {}
